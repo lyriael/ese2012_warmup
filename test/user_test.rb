801cb0d974f8d26cfa68d098d975d25095fea491
lyriael <lyriael@gmx.net>
2012-09-24 23:15:43 +0200

    Tests still don't work
    ...but I need to go to bed. Tomorrow I'll see to it.

--- test/user_test.rb
+++ test/user_test.rb
@@ -1,7 +1,12 @@
-require "test/unit"
-require "../app/Trade_Market/user"
+require 'test/unit'
+require '../app/Trade_Market/user'
 
 class UserTest   < Test::Unit::TestCase
+
+  def should_create_user
+    user = Trade_Market::user.new('Schmendrick')
+    assert(user != nil)
+  end
 
   def test_should_have_name
     user = Trade_Market::user.new('Schmendrick')
@@ -16,15 +21,16 @@ class UserTest   < Test::Unit::TestCase
   def test_should_have_items_list
     user = Trade_Market::user.new('Schmendrick')
     puts user.items.to_s
-    asser(user.items != nil, 'User should have an initialized array, but array is \' nil \'')
+    assert(user.items != nil, 'User should have an initialized array, but array is \' nil \'')
   end
 
   def test_should_have_no_item_yet
     user = Trade_Market::user.new('Schmendrick')
-    asser(user.items.size == 0, 'User should have no items yet, but array contains' + user.items.size + 'items: ' +user.items.to_s)
+    assert(user.items.size == 0, 'User should have no items yet, but array contains' + user.items.size + 'items: ' +user.items.to_s)
   end
 
-  def test_to_test
-    asser(2 != 2)
+  #something is reeeeeally wrong here...
+  def test_to_test_test
+    assert(2==8)
   end
 end
